e25fa9e7d333ad7130eebbe199c49ada87eac471
MohamedAmine1113 <mohamed.amine.bahmane@gmail.com>
2025-03-30 05:52:04 +0000

    Add Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 
 const App = () => {
   return (
-    <div className='box'>
+    <div className='box text-2xl font-bold lowercase' >
       <img src="/public/bricole logo.png" alt="Logo"  />
       <h1>BRICOLE</h1>
     </div>
