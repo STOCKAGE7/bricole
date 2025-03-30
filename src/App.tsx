b98926c1eeb0b1537aa1d402fe38fda79f5acd44
MohamedAmine1113 <mohamed.amine.bahmane@gmail.com>
2025-03-30 05:18:58 +0000

    Add Tool Vite + Typscript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+  return (
+    <div className='box'>
+      <img src="/public/bricole logo.png" alt="Logo"  />
+      <h1>BRICOLE</h1>
+    </div>
+  )
+}
+
+export default App
